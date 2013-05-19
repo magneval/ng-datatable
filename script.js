e1814558dd43d712cdee1f94f2293705e919c2b8
Charles de MAGNEVAL <magneval@gmail.com>
2013-05-19 23:42:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ angular.module('components', []).
       restrict: 'E',
 controller: function($scope, $element) {
 }
+      template:
+        '<div class="tabbable">' +
+          '<ul class="nav nav-tabs">' +
+            '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
+              '<a href="" ng-click="select(pane)">{{pane.title}}</a>' +
+            '</li>' +
+          '</ul>' +
+          '<div class="tab-content" ng-transclude></div>' +
+        '</div>',
+      replace: true
+    };
+  })
 }
 }
