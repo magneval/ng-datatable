366bbb7cce79edb8cba6796fb8a2579072725b63
Charles de MAGNEVAL <magneval@gmail.com>
2013-05-20 23:43:01 +0200

    Update datatable.js

--- js/datatable.js
+++ js/datatable.js
@@ -25,22 +25,6 @@ angular.module('components', [])
     return {
         restrict: 'E',
         transclude: true,
-//      scope: {},
-//      controller: function($scope, $element) {
-//        var panes = $scope.panes = [];
-// 
-//        $scope.select = function(pane) {
-//          angular.forEach(panes, function(pane) {
-//            pane.selected = false;
-//          });
-//          pane.selected = true;
-//        };
-// 
-//        this.addPane = function(pane) {
-//          if (panes.length === 0) $scope.select(pane);
-//          panes.push(pane);
-//        };
-//      },
         template:
                 '<table border="1">' +
                 '<tr >' +
