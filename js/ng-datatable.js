80f1b785bcce46d74e18173ea5b8d0170e2204f9
Charles de MAGNEVAL <magneval@gmail.com>
2013-05-26 21:54:23 +0000

    Update ng-datatable.js

--- js/ng-datatable.js
+++ js/ng-datatable.js
@@ -2,6 +2,10 @@ var ng-datatable = angular.module("components",[])
 
 ng-datatable.directive("ng-datatable", function() {
   return {
+    restrict: 'E',
+    scope: {
+      
+    },
     template: '<div></div>'
   }
 }
