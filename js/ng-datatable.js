8a4be2efc3b3a487d52ac212096895353395237b
Charles de MAGNEVAL <magneval@gmail.com>
2013-05-26 22:00:11 +0000

    Update ng-datatable.js

--- js/ng-datatable.js
+++ js/ng-datatable.js
@@ -4,9 +4,32 @@ ng-datatable.directive("ng-datatable", function() {
   return {
     restrict: 'E',
     scope: {
-      
+      service: "@",
+      configs: "=",
+      onselect: "&"
     },
-    template: '<div></div>'
+    template: 
+                    '<table border="1" >' +
+                    ' <thead>' +
+                    ' <tr>' +
+                    ' <th id="column{{column}}" ng-class="getClass(column)" ng-repeat="column in columns" ng-click="sort(this)">{{column}}</th>' +
+                    ' <th ng-click="export()">export</th>' +
+                    ' </tr>' +
+                    ' </thead>' +
+                    ' <tbody>' +
+                    ' <tr ng-repeat="line in objects">' +
+                    ' <td ng-repeat="column in columns" ng-click="edit(line)">' +
+                    '{{line[column]}}' +
+                    ' </td>' +
+                    ' <td>' +
+                    '<button class="btn btn-danger" ng-click="delete(line, $index, $event)">Delete</button>' +
+                    ' </td>' +
+                    ' </tr>' +
+                    ' </tbody>' +
+                    '</table>',
+    link: function(scope) {
+      scope.datas=service.query(configs);
+    }
   }
 }
 )
